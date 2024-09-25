9352ea810811c3818cbe41d2e842e3b0e7596008
Paromita <paromitatalukder04@gmail.com>
2024-09-25 15:56:51 -0400

    completed in class 4

--- in-class4/in-class-4.js
+++ in-class4/in-class-4.js
@@ -1,1 +1,12 @@
 console.log("in-class-4.js loaded!");
+
+const numbers = [1,2,3,4,5];
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i]%2 == 1) {
+        console.log(`${numbers[i]} is odd!`);
+    }
+    else
+    {
+        console.log(`${numbers[i]} is even!`);
+    }
+}
